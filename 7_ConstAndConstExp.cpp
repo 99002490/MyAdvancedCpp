0e8ac412f9c56be734860ce71c5f1720447b9ced
99002490 <rakshith.m2@ltts.com>
2020-10-21 18:25:57 +0530

    Seventh

---
+++ 7_ConstAndConstExp.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+//int main()
+//{
+//const double gravity { 9.8 };
+//const double g;  error   
+ 
+    //return 0;
+//}
+//void printInteger(const int myValue)
+//{
+    //std::cout << myValue;
+//}
