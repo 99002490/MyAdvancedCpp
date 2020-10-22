65af04b62d8be6b311d556eff5b85fb8e995a40a
99002490 <rakshith.m2@ltts.com>
2020-10-22 14:10:12 +0530

    Tenth

---
+++ 10_StaticCast.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+int i1 { 10 };
+int i2 { 4 };
+ 
+// convert an int to a float so we get floating point division rather than integer division
+float f { static_cast<float>(i1) / i2 }; 
+std::cout<<f<<"\n"; 
+return 0;
+}
