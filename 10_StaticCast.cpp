6bd33dda51f7c653f0c351908ad52855a4df48e1
99002490 <rakshith.m2@ltts.com>
2020-10-23 10:22:08 +0530

    Fourteen

--- 10_StaticCast.cpp
+++ 10_StaticCast.cpp
@@ -4,7 +4,7 @@ int i1 { 10 };
 int i2 { 4 };
  
 // convert an int to a float so we get floating point division rather than integer division
-float f { static_cast<float>(i1) / i2 }; 
+float f { static_cast<float>(i1)/(i2)}; 
 std::cout<<f<<"\n"; 
 return 0;
 }
