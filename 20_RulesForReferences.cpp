2d6199f05cdad7a8dd4487164a4e288521a15c97
99002490 <rakshith.m2@ltts.com>
2020-10-24 15:41:36 +0530

    Twenty

---
+++ 20_RulesForReferences.cpp
@@ -0,0 +1,8 @@
+/*
+a) char is a fundamental type, it should be passed by value.
+b) std::string has to create a copy of the string whenever it is copied. Pass it by const reference.
+c) bool is a fundamental type, it should be passed by value.
+d) int is a fundamental type, it should be passed by value.
+e) Enumerators (Of enum and enum class) are named integers. They are based on a fundamental type (Usually `int`) and should be passed by value.
+f) double is a fundamental type, it should be passed by value.
+*/
